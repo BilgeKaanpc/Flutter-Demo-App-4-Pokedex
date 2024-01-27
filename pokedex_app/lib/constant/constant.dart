7f4b801d18718912789a44fa5710a60a17462577
BilgeKaanpc <kashikoi.dev@gmail.com>
2024-01-27 16:43:14 +0300

    Update constant.dart

--- pokedex_app/lib/constant/constant.dart
+++ pokedex_app/lib/constant/constant.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class Constants {
   Constants._();
 
-  static const String title = "Pokedex";
+  static const String title = "Pokedex Bilge";
   static TextStyle getTitleStyle() {
     return TextStyle(
       color: Colors.white,
